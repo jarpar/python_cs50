d770f17cedb5f988c15442ceb40327527ebfe98e
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-01 17:20:28 +0100

    black clean code playback.py

--- playback/playback.py
+++ playback/playback.py
@@ -3,6 +3,7 @@ prompts the user for input
 outputs that same input, replacing each space with ... (i.e., three periods).
 """
 
+
 def main():
     get_input()
 
@@ -12,5 +13,3 @@ def get_input():
 
 
 main()
-
-
