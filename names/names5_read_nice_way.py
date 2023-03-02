7cbd0e19da9ab393508167c73ff6da46e7932eef
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-02 14:29:50 +0100

    sorted

--- names/names5_read_nice_way.py
+++ names/names5_read_nice_way.py
@@ -1,4 +1,4 @@
 with open("names.txt", "r") as file:
     for line in file:
-        print("hello, ", line.rstrip())
+        print("hello,", line.rstrip())
 
---
+++ names/names6_sorted.py
@@ -0,0 +1,9 @@
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
+
