f63e38689b6251cde1e12d9da811556f06db3166
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-02 18:02:49 +0100

    double variables in place of table(single variable)

--- names/students.py
+++ names/students.py
@@ -1,4 +1,5 @@
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        #row = line.rstrip().split(",")
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
