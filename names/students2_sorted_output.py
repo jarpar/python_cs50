d72d96197994894d4ec1eb36af9ec0e504215b56
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 10:02:01 +0100

    sorted by

--- names/students2_sorted_output.py
+++ names/students2_sorted_output.py
@@ -16,7 +16,25 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
+
+def get_name(student_name):
+    return student_name["name"]
+
+
+def get_house(student_house):
+    return student_house["house"]
+
+
 # for student in sorted(students):
 # print(student)
-for student in students:
+
+print("Sorted by name in alphabetical order:")
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")
+
+print("----")
+print("Sorted by house in non-alphabetical order:")
+
+for student in sorted(students, key=get_house, reverse=True):
     print(f"{student['name']} is in {student['house']}")
