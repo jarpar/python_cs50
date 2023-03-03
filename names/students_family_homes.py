370a4c11201f57aef07ae03abe3c7bd53d8c1494
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 14:13:50 +0100

    module import for csv

---
+++ names/students_family_homes.py
@@ -0,0 +1,10 @@
+import csv
+students = []
+
+with open("students_family_homes.csv") as file:
+    reader = csv.reader(file, delimiter=',', quotechar='"')
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
