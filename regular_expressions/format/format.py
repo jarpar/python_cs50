1f32a200c200dedb142f80eda599cc7deda13cf3
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 21:25:49 +0100

    changed matches for reg exp

---
+++ regular_expressions/format/format.py
@@ -0,0 +1,8 @@
+import re
+
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last, first = matches.groups()
+    name = f"{first} {last}"
+print(f"hello, {name}")
