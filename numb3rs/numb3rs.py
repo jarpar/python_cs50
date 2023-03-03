bd787c568cbdffb2780ada1dc17d5da96401c4b0
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-04 00:52:46 +0100

    added new task

---
+++ numb3rs/numb3rs.py
@@ -0,0 +1,25 @@
+"""
+implement a function called validate
+that expects an IPv4 address as input as a str
+nd then returns True or False
+https://cs50.harvard.edu/python/2022/psets/7/numb3rs/
+"""
+
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
+
