939bcabbacc9145a9a27c1751caa620a4416c380
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 21:06:37 +0100

    added different reg exp

--- regular_expressions/validate/validate.py
+++ regular_expressions/validate/validate.py
@@ -2,6 +2,8 @@ import re
 
 email = input("What's your email? ").strip()
 
+# if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
+
 if re.search(
     r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", email
 ):
