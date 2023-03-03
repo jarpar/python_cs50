b0a50be67bcf3703e0a1abf8e50134f5f7030fca
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 14:48:43 +0100

    csv file keywords reversed

---
+++ names/students_family_homes2_reversed_csv_keywords.py
@@ -0,0 +1,13 @@
+import csv
+
+# remember add to csv file dictionary keys (ex: name, home)
+
+students = []
+
+with open("students_family_homes2_reversed_csv_keywords.csv") as file:
+    reader = csv.DictReader(file, delimiter=",", quotechar='"')
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
