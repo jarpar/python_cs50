46c948968cdf8e7b4b530db4a2dbece4b2d888b9
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 21:32:53 +0100

    grouping changed

--- regular_expressions/format/format.py
+++ regular_expressions/format/format.py
@@ -3,6 +3,8 @@ import re
 name = input("What's your name? ").strip()
 matches = re.search(r"^(.+), (.+)$", name)
 if matches:
-    last, first = matches.groups()
-    name = f"{first} {last}"
+    # last = matches.group(1)
+    # first = matches.group(2)
+    # name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
