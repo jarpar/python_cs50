54f328f7402a8c7d8a5a9c1277c48128db0f2dad
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-03 09:38:28 +0100

    dict

--- names/students2_sorted_output.py
+++ names/students2_sorted_output.py
@@ -3,8 +3,20 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        students.append(f"{name} is in the {house}")
 
+        # v1:
+        # students.append(f"{name} is in the {house}")
 
-for student in sorted(students):
-    print(student)
+        # v2:
+        # student = {}
+        # student["name"] = name
+        # student["house"] = house
+
+        # v3 is the same as v2 but another notation:
+        student = {"name": name, "house": house}
+        students.append(student)
+
+# for student in sorted(students):
+# print(student)
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
