aff6931b72d572545d85713f1a4a56bc4029261b
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-04 11:56:40 +0100

    use of walrus operator

--- regular_expressions/format/format.py
+++ regular_expressions/format/format.py
@@ -1,8 +1,10 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
-if matches:
+# := is called "walrus" operator
+# The walrus operator is a new syntax that is only available in Python 3.8 and later.
+if matches := re.search(r"^(.+), (.+)$", name):
+    # if matches:
     # last = matches.group(1)
     # first = matches.group(2)
     # name = f"{first} {last}"
