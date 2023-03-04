cd2b5eda1edf132637d64cc4e7fd40fd960a4ac4
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-04 22:46:48 +0100

    added test for polish special characters

--- regular_expressions/twitter/twitter.py
+++ regular_expressions/twitter/twitter.py
@@ -10,4 +10,7 @@ url = input("URL: ").strip()
 if matches := re.search(
     r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE
 ):
+# added test for polish special characters:
+#     r"^https?://(?:www\.)?twitter\.com/([a-z0-9_\.ą-ż]+)", url, re.IGNORECASE
+# ):
     print(f"Username: ", matches.group(1))
