76b0b8daf1a3aa3e5b181a9dc2f24a013744a413
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-04 13:05:55 +0100

    fixed reg exp

--- regular_expressions/twitter/twitter.py
+++ regular_expressions/twitter/twitter.py
@@ -1,10 +1,13 @@
 import re
+
 url = input("URL: ").strip()
 
 # username = url.replace("http?s://\.twitter.com/", "")
 # username = re.sub(r"^(https?://)?(www\.)?twitter\.com/?", "", url)
 
 # test input:
-# https://twitter.com/user.name
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+# https://twitter.com/user_name1
+if matches := re.search(
+    r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE
+):
     print(f"Username: ", matches.group(1))
