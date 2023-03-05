04d6dd58cf68ab2e24b2bf489bc5943d3ddbc734
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-05 16:05:30 +0100

    fixed class Student

--- object-oriented_programming/student/student.py
+++ object-oriented_programming/student/student.py
@@ -1,18 +1,17 @@
+class Student:
+    ...
+
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
-    # student = {"name": input(" Name: "), "house": input("House: ")}
-    # student["name"] = input("Name: ")
-    # student["house"] = input("House: ")
-    # return student
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student()
+    student.name = input("Name is: ")
+    student.house = input("House: ")
+    return student
 
 
 if __name__ == "__main__":
--- object-oriented_programming/student2_dict.py
+++ object-oriented_programming/student2_dict.py
@@ -1,15 +1,19 @@
-class Student:
-        ...
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
-    student = Student()
-    student.name = input("Name is: ")
-    student.house = input("House: ")
-    return student
+    # student = {"name": input(" Name: "), "house": input("House: ")}
+    # student["name"] = input("Name: ")
+    # student["house"] = input("House: ")
+    # return student
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
 
 if __name__ == "__main__":
     main()
