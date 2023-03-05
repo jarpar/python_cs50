196c000ff5491d604a1a578b9b20b450e84d830a
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-05 16:01:25 +0100

    class Student

--- object-oriented_programming/student2_dict.py
+++ object-oriented_programming/student2_dict.py
@@ -1,20 +1,15 @@
-class
+class Student:
+        ...
 def main():
-student = get_student()
-if student["name"] == "Padma":
-    student["house"] = "Ravenclaw"
-print(f"{student['name']} from {student['house']}")
+    student = get_student()
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
-    # student = {"name": input(" Name: "), "house": input("House: ")}
-    # student["name"] = input("Name: ")
-    # student["house"] = input("House: ")
-    # return student
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
-
+    student = Student()
+    student.name = input("Name is: ")
+    student.house = input("House: ")
+    return student
 
 if __name__ == "__main__":
     main()
