1bcbc8814661939fff594745719f505329cef90a
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-08 14:12:27 +0100

    added new student2

---
+++ object-oriented_programming/student/student2.py
@@ -0,0 +1,24 @@
+class Student:
+    #...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    # student = Student()
+    # student.name = input("Name is: ")
+    # student.house = input("House: ")
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
+
+
+if __name__ == "__main__":
+    main()
