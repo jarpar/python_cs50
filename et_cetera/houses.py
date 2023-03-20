d5be63e25cd30f11c6ded69bfba6f95c4c122b1c
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-20 17:16:02 +0100

    added dictionary

---
+++ et_cetera/houses.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Hermione", "house": "Grtffindor"},
+    {"name": "Harry", "house": "Grtffindor"},
+    {"name": "Ron", "house": "Grtffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
