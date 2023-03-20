bd2753e0a5c51173581514c01a6876b19c28a025
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-20 17:42:02 +0100

    clean up

---
+++ et_cetera/bank.py
@@ -0,0 +1,10 @@
+balance = 0
+
+
+def main():
+    print("Balance:", balance)
+
+
+if __name__ == "__main__":
+    main()
+
--- et_cetera/houses.py
+++ et_cetera/houses.py
@@ -1,7 +1,17 @@
 students = [
-    {"name": "Hermione", "house": "Grtffindor"},
-    {"name": "Harry", "house": "Grtffindor"},
-    {"name": "Ron", "house": "Grtffindor"},
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
     {"name": "Draco", "house": "Slytherin"},
     {"name": "Padma", "house": "Ravenclaw"},
 ]
+# houses = []
+houses = set()
+for student in students:
+    # if student["house"] not in houses:
+    #     houses.append(student["house"])
+    houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
+
