37258bc6d376b927d7ad6b59d7be6b31ff7218c6
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-30 10:55:24 +0200

    added args reader

--- et_cetera/usage_of_argparse.py
+++ et_cetera/usage_of_argparse.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-n")
 args = parser.parse_args()
 
-for _ in range(int([args.n
+for _ in range(int(args.n)):
+    print("meow")
 
-## stopped @1:04:00
+ 
