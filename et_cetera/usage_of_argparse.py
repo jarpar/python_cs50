e57b5509518a9628fa868713523ecff3163266d6
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-03-30 11:06:01 +0200

    test

--- et_cetera/usage_of_argparse.py
+++ et_cetera/usage_of_argparse.py
@@ -1,10 +1,12 @@
+#python usage_of_argparse.py -n 3
+
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
 args = parser.parse_args()
 
-for _ in range(int(args.n)):
+for _ in range(args.n):
     print("meow")
 
  
