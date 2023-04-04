fd701ffc14956fd2d030e4578fe17022b828be39
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-04 12:58:09 +0200

    test

---
+++ et_cetera/gryffindors.py
@@ -0,0 +1,13 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor"
+
+
+gryffindors = filter(is_gryffindor, students)
