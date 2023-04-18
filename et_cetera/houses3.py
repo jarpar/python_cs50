ba1b715ddd221f6f0917ea7e190a74bd69f16509
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-18 09:56:45 +0200

    cleanup

--- et_cetera/houses3.py
+++ et_cetera/houses3.py
@@ -6,11 +6,12 @@ students = [
     {"name": "Padma", "house": "Ravenclaw"},
 ]
 
-houses = []
+# houses = []
+houses = set()
 for student in students:
-    if student["house"] not in houses:
-        houses.append(student["house"])
+    # if student["house"] not in houses:
+    #     houses.append(student["house"])
+    houses.add(student["house"])
 
 for house in sorted(houses):
     print(house)
-
