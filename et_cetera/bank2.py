93af0e8e6e98e5132a7f4dd030433d18e1b07201
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-18 12:01:25 +0200

    added class Account

---
+++ et_cetera/bank2.py
@@ -0,0 +1,18 @@
+
+def main():
+    balance = 0
+    print("Balance: ", balance)
+    deposit(100)
+    withdraw(50)
+    print("Balance: ", balance)
+
+def deposit(n):
+    global balance
+    balance += n
+
+def withdraw(n):
+    global balance
+    balance -= n
+
+if __name__ == "__main__":
+    main()
---
+++ et_cetera/bank3.py
@@ -0,0 +1,23 @@
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    def balance(self):
+        return self.balance()
+
+    def deposit(self,n):
+        self.balance() += n
+
+    def withdraw(self,n):
+        self.balance() -= n
+
+def main():
+    account = Account()
+    print("Balance:", account.balance())
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance", account.balance()
+
+if __name__ == "__main__":
+    main()
+
